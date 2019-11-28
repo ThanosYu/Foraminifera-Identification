faff278c2892848c1aaf8a50a33c42444b707878
Yu Shi Qiang <shiqiang.yu@siemens.com>
2019-11-28 14:30:33 +0800

    resample

--- ReSampleDataFeature.py
+++ ReSampleDataFeature.py
@@ -12,8 +12,13 @@ session = cluster.connect(keyspace='dex')
 cql = 'select * from data_feature where sensor_id = %s AND type = %s allow filtering'
 result = session.execute(cql, ('S1MTR1001AC135RN', 'DKW')).current_rows
 df = pd.DataFrame(result)
-df = df[(df['time'] >= 1536903994000) & (df['time'] <= 1568270649060)]
-df1 = df.value.resample('1S').mean()
+df = df[(df['time'] >= 1536903994000) & (df['time'] <= 1568270649000)]
+# df['value'] = pd.to_numeric(df['value'])
+df['time'] = pd.to_datetime(df['time'], unit='ms')
 
+df = df.set_index('time')
+df['value'] = df['value'].resample('1S').ffill()
+# if lack one minute, will it fill it ?
+print('bajdnflsf ', df['value'])
 
-print(df)
+# print(df)
---
+++ ResampleTest.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# pd.set_option('display.mpl_style', 'default')
+
+range = pd.date_range('2015-01-01', '2015-12-31', freq='15min')
+df = pd.DataFrame(index=range)
+
+# Average speed in miles per hour
+df['speed'] = np.random.randint(low=0, high=60, size=len(df.index))
+# Distance in miles (speed * 0.5 hours)
+df['distance'] = df['speed'] * 0.25
+# Cumulative distance travelled
+df['cumulative_distance'] = df.distance.cumsum()
+print(df)
+# fig, ax1 = plt.subplots()
+#
+# ax2 = ax1.twinx()
+# ax1.plot(df.index, df['speed'], 'g-')
+# ax2.plot(df.index, df['distance'], 'b-')
+#
+# ax1.set_xlabel('Date')
+# ax1.set_ylabel('Speed', color='g')
+# ax2.set_ylabel('Distance', color='b')
+#
+# plt.show()
+# plt.rcParams['figure.figsize'] = 12, 5
+
+weekly_summary = pd.DataFrame()
+weekly_summary['speed'] = df.speed.resample('W').mean()
+weekly_summary['distance'] = df.distance.resample('W').sum()
+weekly_summary['cumulative_distance'] = df.cumulative_distance.resample('W').last()
+
+# Select only whole weeks
+weekly_summary = weekly_summary.truncate(before='2015-01-05', after='2015-12-27')
+weekly_summary.head()
+
+fig, ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+ax1.plot(weekly_summary.index, weekly_summary['speed'], 'g-')
+ax2.plot(weekly_summary.index, weekly_summary['distance'], 'b-')
+
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Speed', color='g')
+ax2.set_ylabel('Distance', color='b')
+
+plt.show()
+plt.rcParams['figure.figsize'] = 12, 5
