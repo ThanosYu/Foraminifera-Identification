860cccc0fcad00fbbdf1538f8a257e05112c2461
Yu Shi Qiang <shiqiang.yu@siemens.com>
2019-12-03 15:53:20 +0800

    save

--- ReSampleDataFeature.py
+++ ReSampleDataFeature.py
@@ -17,7 +17,7 @@ df['value'] = df['value'].astype(pd.np.float64)
 df['time'] = pd.to_datetime(df['time'], unit='ms')
 print('===========start resample')
 df = df.resample('1H', on='time').mean().ffill()
-
+print(df)
 df.reset_index(level=0, inplace=True)
 
 df['time'] = (df['time'].values - np.datetime64('1970-01-01T08:00:00Z')) / np.timedelta64(1, 'ms')
@@ -25,12 +25,12 @@ df['time'] = df['time'].astype(pd.np.int64)
 df['start_time'] = df['start_time'].astype(pd.np.int64)
 df['end_time'] = df['end_time'].astype(pd.np.int64)
 
-print('===========start insert')
-for row in range(len(df)):
-    print(df.loc[row, 'value'])
-    print('%.19f' % (df.loc[row, 'value']))
-    # insertSql = 'insert into data_feature_resample(sensor_id,type,time,end_time,start_time,value) values (%s,%s,%s,' \
-    #             '%s,%s,%s) '
-    # session.execute(insertSql,
-    #                 ('S1MTR1001AC135RN', 'DKW', df.loc[row, 'time'], df.loc[row, 'end_time'], df.loc[row, 'start_time'],
-    #                  df.loc[row, 'value']))
+# print('===========start insert')
+# for row in range(len(df)):
+#     print(df.loc[row, 'value'])
+#     print('%.19f' % (df.loc[row, 'value']))
+#     insertSql = 'insert into data_feature_resample(sensor_id,type,time,end_time,start_time,value) values (%s,%s,%s,' \
+#                 '%s,%s,%s) '
+#     session.execute(insertSql,
+#                     ('S1MTR1001AC135RN', 'DKW', df.loc[row, 'time'], df.loc[row, 'end_time'], df.loc[row, 'start_time'],
+#                      df.loc[row, 'value']))
